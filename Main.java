b17a6e0e33b4bbe820ec9f5ab16914060d1687ce
subhashdoc234xyz <subhashdoc234@gmail.com>
2026-03-08 23:02:19 +0530

    loging changed and animation upgraded

--- Main.java
+++ Main.java
@@ -1,17 +1,9 @@
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        // Loop runs exactly 10 times
-        for (int i = 1; i <= 10; i++) {
-            System.out.print("Enter integer #" + i + ": ");
-            int number = input.nextInt(); // Captures the input
-            
-            System.out.println("You entered: " + number);
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Number: " + i);
+            i++;
         }
-        
-        input.close(); // Good practice to close the scanner
     }
 }
