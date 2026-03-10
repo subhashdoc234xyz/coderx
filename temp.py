ffb8edd296e61dcbc620a34877a0ea3125b4039e
subhashdoc234xyz <subhashdoc234@gmail.com>
2026-03-10 19:33:45 +0530

    input method changed

--- temp.py
+++ temp.py
@@ -1,5 +1,6 @@
-a= int(input("Enter you Age : "))
-if(a<18):
-     print("ok")
-else:
-     print("no")
+# Loop through the first 10 user inputs
+for i in range(10):
+    # Ask the user to enter an integer
+    number = int(input(f"Enter integer {i+1}: "))
+    # Print the entered integer
+    print(number)
